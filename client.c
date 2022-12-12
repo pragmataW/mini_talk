17e61d66afb5fe371e5e3b45f411a23f8eb9795b
Yusuf Ciftci <yciftci@k2m05s01.42kocaeli.com.tr>
2022-12-13 01:14:39 +0300

    unfinished yet

---
+++ client.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yciftci <yciftci@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 18:28:51 by yciftci           #+#    #+#             */
+/*   Updated: 2022/12/13 01:05:54 by yciftci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_talk.h"
+
+void	get_bit(int pid, char c)
+{
+	int	bit;
+	int	n;
+	int	i;
+
+	n = 7;
+	i = 0;
+	while (n >= 0)
+	{
+		bit = (c >> n) & 1;
+		if (bit == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(150);
+	}
+}
+
+void	get_char(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		get_bit(pid, str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	pid;
+
+	if (argc < 3)
+		ft_putstr_fd(LOWARGERR, 1);
+	if (argc > 3)
+		ft_putstr_fd(TOOARGERR, 1);
+	pid = atoi(argv[1]);
+	get_char(pid, argv[2])
+	return (0);
+}
---
+++ mini_talk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_talk.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yciftci <yciftci@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 01:55:20 by yciftci           #+#    #+#             */
+/*   Updated: 2022/12/13 00:39:40 by yciftci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_TALK_H
+# define MINI_TALK_H
+
+# define LOWARGERR "Too Few Arguments!"
+# define TOOARGERR "Too Many Arguments!"
+
+# include <unistd.h>
+# include <stlib.h>
+# include <signal.h>
+# include "libft/libft.h"
+
+#endif
